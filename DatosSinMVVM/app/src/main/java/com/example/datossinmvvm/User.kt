b837971fb09ea5a6577790f1dec6428657c42978
fatux <arianna.chavez@tecsup.edu.pe>
2025-10-05 16:32:01 -0500

    Creación User.kt

---
+++ DatosSinMVVM/app/src/main/java/com/example/datossinmvvm/User.kt
@@ -0,0 +1,13 @@
+package com.example.datossinmvvm
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ColumnInfo
+
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+)
+
